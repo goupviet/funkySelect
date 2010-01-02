97a768606dc55d1472bd8527e8fd557d21b261f2
Doug Neiner <doug@Doug-Neiners-MacBook-Pro.local>
2010-01-02 13:05:25 -0500

    Added the support for global options with the ability to override on a per call basis.
    Replaced all `jquery-funkyselect-` classes with a shared variable `cl.name` format. This allows all the class names to be overwritten with a different prefix if needed.

--- jquery.funkySelect.js
+++ jquery.funkySelect.js
@@ -11,17 +11,33 @@
 
 (function($) {
 
-    $.fn.funkySelect = function() {
+    $.fn.funkySelect = function( options ) {
         
-        var $s, $fs, header, sOptions, selected, funkySelect;
+        var $s, $fs, header, sOptions, selected, funkySelect, o, cl;
+        
+        // Merge default options with the `options` parameter
+        o = $.extend({}, $.fn.funkySelect.defaults, options ); // Allow defaults to be overwritten
+        
+        // Define classes
+        cl = {
+            parent:      [o.classPrefix, 'container'   ].join('-'),
+            header:      [o.classPrefix, 'header'      ].join('-'),
+            open:        [o.classPrefix, 'open'        ].join('-'),
+            corners:     [o.classPrefix, 'corners'     ].join('-'),
+            selected:    [o.classPrefix, 'selected'    ].join('-'),
+            arrow:       [o.classPrefix, 'arrow'       ].join('-'),
+            options:     [o.classPrefix, 'options'     ].join('-'),
+            option:      [o.classPrefix, 'option'      ].join('-'),
+            optiongroup: [o.classPrefix, 'optiongroup' ].join('-')
+        };
         
         funkySelect = [
-            '<div class="jquery-funkyselect">',
-                '<div class="jquery-funkyselect-header jquery-funkyselect-corners">',
-                    '<span class="jquery-funkyselect-selected"></span>',
-                    '<span class="jquery-funkyselect-arrow jquery-funkyselect-corners"><span></span></span>',
+            '<div class="', o.classPrefix ,'">',
+                '<div class="', cl.header, ' ', cl.corners, '">',
+                    '<span class="', cl.selected, '"></span>',
+                    '<span class="', cl.arrow, ' ', cl.corners, '"><span></span></span>',
                 '</div>',
-                '<div class="jquery-funkyselect-options jquery-funkyselect-corners">',
+                '<div class="', cl.options , ' ', cl.corners, '">',
                     '<ul></ul>',
                 '</div>',
             '</div>'
@@ -29,38 +45,34 @@
         
         
         function bindEvents() {
-            var parentClass = 'jquery-funkyselect-container',
-                headerClass = 'jquery-funkyselect-header',
-                optionClass = 'jquery-funkyselect-option',
-                openClass = 'jquery-funkyselect-open';
             
             $('html').bind('click', function(e) {
                 var $this, $cont, target = $(e.target);
                
                // on click outside of funkySelect boxes
-               if (target.parents('.' + parentClass).length === 0) {
-                   $('.' + parentClass).removeClass(openClass);
+               if (target.parents('.' + cl.parent).length === 0) {
+                   $('.' + cl.parent).removeClass(cl.open);
                }
                
                // on funkySelect header click
-               if (target.hasClass(headerClass) || target.parents('.' + headerClass).length) {
-                   $cont = target.parents('.' + parentClass);
+               if (target.hasClass(cl.header) || target.parents('.' + cl.header).length) {
+                   $cont = target.parents('.' + cl.parent);
 
-                   if($cont.hasClass(openClass)) {
-                       $cont.removeClass(openClass);
+                   if($cont.hasClass(cl.open)) {
+                       $cont.removeClass(cl.open);
                    } else {
-                       $('.' + parentClass).removeClass(openClass);
-                       $cont.addClass(openClass);
+                       $('.' + cl.parent).removeClass(cl.open);
+                       $cont.addClass(cl.open);
                    }                   
                }
                
                // on funkySelect option click
-               if (target.hasClass(optionClass) || target.parents('.' + optionClass).length) {
-                   $this = target.hasClass(optionClass) ? target : target.parents('.' + optionClass);
-                   $cont = target.parents('.' + parentClass);
+               if (target.hasClass(cl.option) || target.parents('.' + cl.option).length) {
+                   $this = target.hasClass(cl.option) ? target : target.parents('.' + cl.option);
+                   $cont = target.parents('.' + cl.parent);
 
-                   $cont.find('.jquery-funkyselect-selected').text(target.text());
-                   $cont.find('.' + headerClass).trigger('click');
+                   $cont.find('.' + cl.selected).text(target.text());
+                   $cont.find('.' + cl.header).trigger('click');
 
                    $cont.find('option').removeAttr('selected');
                    $cont.find('.' + $this.attr('rel')).attr('selected', 'selected');
@@ -69,17 +81,16 @@
             });
 
         }
-        
 
         function adjustDimensions() {
 			
             // adjust height
-            selected.css('line-height', header.height());
+            selected.css('line-height', header.height() + "px");
 			
             // adjust width
-            var optsWidth = sOptions.outerWidth(),
-                arrowWidth = header.find('.jquery-funkyselect-arrow').eq(0).outerWidth(true),
-                width = optsWidth + arrowWidth;
+            var optsWidth  = sOptions.outerWidth(),
+                arrowWidth = header.find('.' + cl.arrow ).eq(0).outerWidth(true),
+                width      = optsWidth + arrowWidth;
             
             sOptions.css('width', width);
 			
@@ -90,7 +101,6 @@
             $fs.css('width', width);
 						
         }
-        
         
         function prepareMe() {
 			
@@ -103,13 +113,13 @@
                 addOption = function(el, i) {
                     el.addClass('opt' + i);
 
-                    html.push('<li class="jquery-funkyselect-option jquery-funkyselect-corners" rel="opt' + i + '">');
+                    html.push('<li class="' + cl.option + ' ' + cl.corners + '" rel="opt' + i + '">');
                     html.push(el.text());
                     html.push('</li>');
                 };
                 
                 if(this.tagName.toLowerCase() === 'optgroup') {
-                    html.push('<li class="jquery-funkyselect-optiongroup jquery-funkyselect-corners">');
+                    html.push('<li class="' + cl.optiongroup + ' ' + cl.corners + '">');
                     html.push($this.attr('label'));
                     html.push('<ul>');
                     
@@ -139,18 +149,22 @@
         return this.each(function() {
 	
             $s = $(this);
-            $s.wrap('<div class="jquery-funkyselect-container"></div>');
+            $s.wrap('<div class="' + cl.parent + '"></div>');
             
             $fs = $s.parent();
             $fs.prepend(funkySelect.join(''));
 
-            sOptions = $fs.find('.jquery-funkyselect-options').eq(0);
-            header = $fs.find('.jquery-funkyselect-header').eq(0);
-            selected = $fs.find('.jquery-funkyselect-selected').eq(0);
+            sOptions = $fs.find('.' + cl.options ).eq(0);
+            header   = $fs.find('.' + cl.header  ).eq(0);
+            selected = $fs.find('.' + cl.selected).eq(0);
 
             prepareMe();
 
         });
+    };
+    
+    $.fn.funkySelect.defaults = {
+        classPrefix: 'jquery-funkyselect'
     };
 
 })(jQuery);
